df292229efcec7f8ac31e1b858e3049c31ad5cbc
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2025-09-23 20:15:49 +0300

    full

--- src/app.js
+++ src/app.js
@@ -1,2 +1,46 @@
-import "./js/task-one";
-import "./js/task-two"
+const input = document.querySelector("#bookmarkInput");
+const addBtn = document.querySelector("#addBookmarkBtn");
+const listRef = document.querySelector("#bookmarkList");
+
+let urlList = [];
+
+const render = function () {
+  listRef.innerHTML = urlList
+    .map(
+      (url, index) =>
+        `<li>
+      <a target="_blank" href="${url}">${url}</a>
+      <button type="button" data-index="${index}" class="delete">Del</button>
+      <button type="button" data-index="${index}" class="edit">Edit</button>
+    </li>`
+    )
+    .join("");
+};
+
+addBtn.addEventListener("click", function () {
+  const url = input.value.trim();
+  if (url) {
+    urlList.push(url);
+    input.value = "";
+    render();
+  }
+});
+
+listRef.addEventListener("click", (event) => {
+  const index = event.target.dataset.index;
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  if (event.target.classList.contains("edit")) {
+    const newUrl = prompt("Редагуйте посилання", urlList[index]);
+    if (newUrl) {
+      urlList[index] = newUrl;
+      render();
+    }
+  }
+
+  if (event.target.classList.contains("delete")) {
+    urlList.splice(index, 1);
+    render();
+  }
+});
